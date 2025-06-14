52fcdbe3155baca426b04f2f168e0f167a00190f
Selim Han ÇİL <166745238+selimhancil@users.noreply.github.com>
2025-06-14 21:48:26 +0300

    algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,44 +1,21 @@
-# algorithm.py
-
 import heapq
 
-def prim(graph, start_node):
+def prim(graph, start):
     visited = set()
     mst = []
     total_weight = 0
-    min_heap = [(0, start_node, None)]  
+    edges = [(0, start, start)]
 
-    while min_heap:
-        weight, current, prev = heapq.heappop(min_heap)
-
-        if current in visited:
+    while edges:
+        weight, u, v = heapq.heappop(edges)
+        if v in visited:
             continue
-
-        visited.add(current)
-
-        if prev is not None:
-            mst.append((prev, current, weight))
+        visited.add(v)
+        if u != v:
+            mst.append((u, v, weight))
             total_weight += weight
-
-        for neighbor, edge_weight in graph[current]:
+        for neighbor, w in graph.get(v, []):
             if neighbor not in visited:
-                heapq.heappush(min_heap, (edge_weight, neighbor, current))
+                heapq.heappush(edges, (w, v, neighbor))
 
     return mst, total_weight
-
-
-# Test kısmı
-if __name__ == "__main__":
-    graph = {
-        "A": [("B", 3), ("C", 1)],
-        "B": [("A", 3), ("D", 4)],
-        "C": [("A", 1), ("D", 2)],
-        "D": [("B", 4), ("C", 2)]
-    }
-
-    mst, total = prim(graph, "C")
-    print("Minimum Spanning Tree:")
-    for u, v, w in mst:
-        print(f"{u} -- {v} == {w}")
-    print(f"Toplam Maliyet: {total}")
-
